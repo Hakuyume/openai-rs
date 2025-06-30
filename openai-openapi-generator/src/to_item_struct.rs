cf5c78529522ad32dfdcbd59524223197aa97fc4
Toru Ogawa <Hakuyume@users.noreply.github.com>
2025-06-30 10:07:37 +0900

    use derive macro for struct

--- openai-openapi-generator/src/to_item_struct.rs
+++ openai-openapi-generator/src/to_item_struct.rs
@@ -102,9 +102,11 @@ pub fn to_item_struct(
         });
     }
 
-    {
-        let fields_inner = fields.iter().map(
+    if fields.iter().all(|FieldInfo { public, .. }| *public) {
+        let fields = fields.iter().map(
             |FieldInfo {
+                 default,
+                 description,
                  ident,
                  name,
                  nullable,
@@ -113,66 +115,7 @@ pub fn to_item_struct(
                  type_,
                  ..
              }| {
-                let attr_serde_as = serde_as.as_ref().map(|serde_as| {
-                    let serde_as = if *nullable || *optional {
-                        format!("Option<{serde_as}>")
-                    } else {
-                        serde_as.clone()
-                    };
-                    quote::quote!(#[serde_as(as = #serde_as)])
-                });
-                let attr_serde_rename = quote::quote!(#[serde(rename = #name)]);
-                let type_ = if *nullable || *optional {
-                    quote::quote!(Option<#type_>)
-                } else {
-                    quote::quote!(#type_)
-                };
-                quote::quote! {
-                    #attr_serde_as
-                    #attr_serde_rename
-                    #[allow(dead_code)]
-                    #ident: #type_
-                }
-            },
-        );
-        let idents_outer = fields
-            .iter()
-            .filter_map(|FieldInfo { ident, public, .. }| public.then_some(ident));
-        let field_values_outer = fields
-            .iter()
-            .filter_map(|FieldInfo { ident, public, .. }| public.then_some(ident));
-        items.push(syn::parse_quote! {
-            impl<'de> serde::Deserialize<'de> for #ident {
-                fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-                where D: serde::Deserializer<'de> {
-                    #[serde_with::serde_as]
-                    #[derive(serde::Deserialize)]
-                    struct #ident {
-                        #(#fields_inner),*
-                    }
-                    let #ident {
-                        #(#idents_outer,)*
-                        ..
-                    } = #ident::deserialize(deserializer)?;
-                    Ok(Self {
-                        #(#field_values_outer),*
-                    })
-                }
-            }
-        });
-    }
-
-    {
-        let fields_inner = fields.iter().map(
-            |FieldInfo {
-                 ident,
-                 name,
-                 nullable,
-                 optional,
-                 serde_as,
-                 type_,
-                 ..
-             }| {
+                let description = to_description(*description);
                 let attr_serde_as = serde_as.as_ref().map(|serde_as| {
                     let serde_as = if *nullable || *optional {
                         format!("Option<{serde_as}>")
@@ -184,60 +127,6 @@ pub fn to_item_struct(
                 let attr_serde_rename = quote::quote!(#[serde(rename = #name)]);
                 let attr_serde_skip_serializing_if = (*optional || *nullable)
                     .then_some(quote::quote!(#[serde(skip_serializing_if = "Option::is_none")]));
-                let type_ = if *nullable || *optional {
-                    quote::quote!(Option<#type_>)
-                } else {
-                    quote::quote!(#type_)
-                };
-                quote::quote! {
-                    #attr_serde_as
-                    #attr_serde_rename
-                    #attr_serde_skip_serializing_if
-                    #ident: &'a #type_
-                }
-            },
-        );
-        let idents_outer = fields
-            .iter()
-            .filter_map(|FieldInfo { ident, public, .. }| public.then_some(ident));
-        let field_values_inner = fields.iter().map(|FieldInfo { ident, public, .. }| {
-            if *public {
-                quote::quote!(#ident)
-            } else {
-                quote::quote!(#ident: &Default::default())
-            }
-        });
-        items.push(syn::parse_quote! {
-            impl serde::Serialize for #ident {
-                fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-                where S: serde::Serializer {
-                    #[serde_with::serde_as]
-                    #[derive(serde::Serialize)]
-                    struct #ident<'a> {
-                        #(#fields_inner),*
-                    }
-                    let Self {
-                        #(#idents_outer),*
-                    } = self;
-                    #ident { #(#field_values_inner),* }.serialize(serializer)
-                }
-            }
-        });
-    }
-
-    {
-        let fields = fields.iter().filter_map(
-            |FieldInfo {
-                 default,
-                 description,
-                 ident,
-                 nullable,
-                 optional,
-                 public,
-                 type_,
-                 ..
-             }| {
-                let description = to_description(*description);
                 let attr_builder = (*default || *nullable || *optional)
                     .then_some(quote::quote!(#[builder(default)]));
                 let type_ = if *nullable || *optional {
@@ -245,19 +134,185 @@ pub fn to_item_struct(
                 } else {
                     quote::quote!(#type_)
                 };
-                public.then_some(quote::quote! {
+                quote::quote! {
                     #description
+                    #attr_serde_as
+                    #attr_serde_rename
+                    #attr_serde_skip_serializing_if
                     #attr_builder
                     #vis #ident: #type_
-                })
+                }
             },
         );
         syn::parse_quote! {
             #description
             #derive
+            #[serde_with::serde_as]
+            #[derive(serde::Deserialize, serde::Serialize)]
             #[derive(typed_builder::TypedBuilder)]
             #vis struct #ident {
                 #(#fields),*
+            }
+        }
+    } else {
+        {
+            let fields_inner = fields.iter().map(
+                |FieldInfo {
+                     ident,
+                     name,
+                     nullable,
+                     optional,
+                     serde_as,
+                     type_,
+                     ..
+                 }| {
+                    let attr_serde_as = serde_as.as_ref().map(|serde_as| {
+                        let serde_as = if *nullable || *optional {
+                            format!("Option<{serde_as}>")
+                        } else {
+                            serde_as.clone()
+                        };
+                        quote::quote!(#[serde_as(as = #serde_as)])
+                    });
+                    let attr_serde_rename = quote::quote!(#[serde(rename = #name)]);
+                    let type_ = if *nullable || *optional {
+                        quote::quote!(Option<#type_>)
+                    } else {
+                        quote::quote!(#type_)
+                    };
+                    quote::quote! {
+                        #attr_serde_as
+                        #attr_serde_rename
+                        #[allow(dead_code)]
+                        #ident: #type_
+                    }
+                },
+            );
+            let idents_outer = fields
+                .iter()
+                .filter_map(|FieldInfo { ident, public, .. }| public.then_some(ident));
+            let field_values_outer = fields
+                .iter()
+                .filter_map(|FieldInfo { ident, public, .. }| public.then_some(ident));
+            items.push(syn::parse_quote! {
+                impl<'de> serde::Deserialize<'de> for #ident {
+                    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+                    where D: serde::Deserializer<'de> {
+                        #[serde_with::serde_as]
+                        #[derive(serde::Deserialize)]
+                        struct #ident {
+                            #(#fields_inner),*
+                        }
+                        let #ident {
+                            #(#idents_outer,)*
+                            ..
+                        } = #ident::deserialize(deserializer)?;
+                        Ok(Self {
+                            #(#field_values_outer),*
+                        })
+                    }
+                }
+            });
+        }
+
+        {
+            let fields_inner = fields.iter().map(
+                |FieldInfo {
+                     ident,
+                     name,
+                     nullable,
+                     optional,
+                     serde_as,
+                     type_,
+                     ..
+                 }| {
+                    let attr_serde_as = serde_as.as_ref().map(|serde_as| {
+                        let serde_as = if *nullable || *optional {
+                            format!("Option<{serde_as}>")
+                        } else {
+                            serde_as.clone()
+                        };
+                        quote::quote!(#[serde_as(as = #serde_as)])
+                    });
+                    let attr_serde_rename = quote::quote!(#[serde(rename = #name)]);
+                    let attr_serde_skip_serializing_if = (*optional || *nullable).then_some(
+                        quote::quote!(#[serde(skip_serializing_if = "Option::is_none")]),
+                    );
+                    let type_ = if *nullable || *optional {
+                        quote::quote!(Option<#type_>)
+                    } else {
+                        quote::quote!(#type_)
+                    };
+                    quote::quote! {
+                        #attr_serde_as
+                        #attr_serde_rename
+                        #attr_serde_skip_serializing_if
+                        #ident: &'a #type_
+                    }
+                },
+            );
+            let idents_outer = fields
+                .iter()
+                .filter_map(|FieldInfo { ident, public, .. }| public.then_some(ident));
+            let field_values_inner = fields.iter().map(|FieldInfo { ident, public, .. }| {
+                if *public {
+                    quote::quote!(#ident)
+                } else {
+                    quote::quote!(#ident: &Default::default())
+                }
+            });
+            items.push(syn::parse_quote! {
+                impl serde::Serialize for #ident {
+                    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+                    where S: serde::Serializer {
+                        #[serde_with::serde_as]
+                        #[derive(serde::Serialize)]
+                        struct #ident<'a> {
+                            #(#fields_inner),*
+                        }
+                        let Self {
+                            #(#idents_outer),*
+                        } = self;
+                        #ident { #(#field_values_inner),* }.serialize(serializer)
+                    }
+                }
+            });
+        }
+
+        {
+            let fields = fields.iter().filter_map(
+                |FieldInfo {
+                     default,
+                     description,
+                     ident,
+                     nullable,
+                     optional,
+                     public,
+                     type_,
+                     ..
+                 }| {
+                    let description = to_description(*description);
+                    let attr_builder = (*default || *nullable || *optional)
+                        .then_some(quote::quote!(#[builder(default)]));
+                    let type_ = if *nullable || *optional {
+                        quote::quote!(Option<#type_>)
+                    } else {
+                        quote::quote!(#type_)
+                    };
+                    public.then_some(quote::quote! {
+                        #description
+                        #attr_builder
+                        #vis #ident: #type_
+                    })
+                },
+            );
+            syn::parse_quote! {
+                #description
+                #derive
+                #[derive(typed_builder::TypedBuilder)]
+                #vis struct #ident {
+                    #(#fields),*
+                }
             }
         }
     }
